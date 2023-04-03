b7cd9a5e408d58b592a5a7922f7777a6dbf14ab1
rizwanrasheed238 <rizwanrasheed238@gmail.com>
2023-04-03 15:25:49 +0530

    first commit

--- seller/views.py
+++ seller/views.py
@@ -4,7 +4,7 @@ from itertools import product
 from django.shortcuts import render, redirect
 from django.contrib import messages, auth
 from seller.models import seller_product
-from antiqueapp.models import Category
+from antiqueapp.models import Category,product
 from django.urls import reverse
 
 
@@ -17,31 +17,28 @@ def seller(request):
     return render(request, "seller.html", {'products': products})
 
 def addproduct(request):
-    category = Category.objects.all()
-    user = request.user.id
+    categories = Category.objects.all()
     if request.method == "POST":
-        cate = request.POST.get('cate')
+        category_id = request.POST.get('cate')
+        category = Category.objects.get(name=category_id)
+        print(category,"***********************************************")
         pname = request.POST.get('pname')
         pdesc = request.POST.get('pdesc')
         pimg = request.POST.get('pimg')
         price = request.POST.get('price')
-        # color = request.POST.get('color')
-        # size = request.POST.get('size')
         stock = request.POST.get('stock')
-        # is_active = request.POST.get('isactive')
-        # in_stock = request.POST.get('instock')
-        # user = request.user.id
-        # val = Seller_product.objects.all()
-        # user=request.user.id
-        val = seller_product(
-             user_id=user,category=cate, name=pname, descripton=pdesc, image=pimg, price=price, stock=stock
+        new_product =product(
+            name=pname,
+            descripton=pdesc,
+            image=pimg,
+            price=price,
+            category=category,
+            stock=stock,
         )
-        val.save()
-        # add = seller_product.objects.filter()
-        # print(cate,pname,pdesc,pimg,price,stock)
+        new_product.save()
         return redirect('seller')
 
-    return render(request, "addproduct.html",{'category':category})
+    return render(request, "addproduct.html", {'categories': categories})
 
 def viewproduct(request):
     products = seller_product.objects.filter(user_id=request.user)
